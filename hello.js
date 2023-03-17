a1f0fe2ed3de4393722ccd8824e2a2d8e3fbb094
him2109 <128088975+him2109@users.noreply.github.com>
2023-03-16 21:19:41 -0400

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,8 +1,6 @@
-// Define the Web Data Connector
 (function() {
   var myConnector = tableau.makeConnector();
 
-  // Define the schema for the data
   myConnector.getSchema = function(schemaCallback) {
     var cols = [
       { id: "seriesID", alias: "Series ID", dataType: tableau.dataTypeEnum.string },
@@ -21,36 +19,36 @@
     schemaCallback([tableSchema]);
   };
 
-  // Define the connector logic
   myConnector.getData = function(table, doneCallback) {
-    var apiUrl = "https://api.bls.gov/publicAPI/v2/timeseries/data/";
+    var connectionData = JSON.parse(tableau.connectionData),
+        seriesID = connectionData.seriesID,
+        startYear = connectionData.startYear,
+        endYear = connectionData.endYear,
+        apiUrl = "https://api.bls.gov/publicAPI/v2/timeseries/data/";
 
-    // Prompt the user for input
-    tableau.connectionData = JSON.stringify({
-      seriesID: tableau.prompt("Enter the series ID(s) (comma-separated):").split(","),
-      startYear: tableau.prompt("Enter the start year:"),
-      endYear: tableau.prompt("Enter the end year:")
-    });
+    var dataRequest = {
+      "seriesid": seriesID,
+      "startyear": startYear,
+      "endyear": endYear,
+      "registrationkey": "c8c85d09ef3b4435ae40270b1efeeb55"
+    };
 
-    // Make the API request
     $.ajax({
       url: apiUrl,
       type: "POST",
-      data: tableau.connectionData,
-      beforeSend: function(xhr) {
-        xhr.setRequestHeader("Content-type", "application/json");
-      },
+      data: JSON.stringify(dataRequest),
+      contentType: "application/json",
+      dataType: "json",
       success: function(data) {
         var tableData = [];
 
-        // Parse the response data and add to the table
         $.each(data.Results.series, function(i, series) {
           var seriesID = series.seriesID;
           $.each(series.data, function(j, data) {
             var year = data.year,
-              period = data.period,
-              value = data.value,
-              footnotes = data.footnotes[0] ? data.footnotes[0].text : "";
+                period = data.period,
+                value = data.value,
+                footnotes = data.footnotes[0] ? data.footnotes[0].text : "";
             if (period.startsWith("M")) {
               tableData.push({
                 seriesID: seriesID,
@@ -63,17 +61,15 @@
           });
         });
 
-        // Add the data to the table and finish
         table.appendRows(tableData);
         doneCallback();
       },
       error: function(xhr, ajaxOptions, thrownError) {
-        tableau.log(xhr.responseText);
+        console.log(xhr.responseText);
         doneCallback();
       }
     });
   };
 
-  // Register the connector with Tableau
   tableau.registerConnector(myConnector);
 })();
